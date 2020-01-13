102654eb47438a144fcf548ffac68a1e1e369fcb
ryvanasuthelie <sryvana123@gmail.com>
2020-01-13 18:53:24 +0700

    Init

---
+++ src/kamus_bahasa/Kamus_bahasa.java
@@ -0,0 +1,11 @@
+package kamus_bahasa;
+
+public class Kamus_bahasa {
+
+    public static void main(String[] args) {
+        Kamus kms = new Kamus();
+        kms.setLocationRelativeTo(null);
+        kms.setVisible(true);
+    }
+    
+}
